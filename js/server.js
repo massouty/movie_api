98de7272fc190a2862755df24213b1451450b663
massouty <massouty@outlook.com>
2022-08-11 10:37:46 +0200

    Update server.js
    if applied , this commit will have reedited file (server.js)

--- js/server.js
+++ js/server.js
@@ -1,20 +1,46 @@
-const http = require('http');
+/*const http = require('http');
 
 http.createServer((request, response) => {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end('Hello Node!\n');
 }).listen(8080);
 
-console.log('My first Node test server is running on Port 8080.');
+console.log('My first Node test server is running on Port 8080.');*/
 
-const url = require('url');
-let addr = 'http://localhost:8080/default.html?year=2017&month=february';
-let q = url.parse(addr, true);
+const http = require('http'),
+  fs = require('fs'),
+  url = require('url');
 
-console.log(q.host); // returns 'localhost:8080'
-console.log(q.pathname); // returns '/default.html'
-console.log(q.search); // returns '?year=2017&month=february'
+http.createServer((request, response) => {
+  let addr = request.url,
+    q = url.parse(addr, true),
+    filePath = '';
 
-let qdata = q.query; // returns an object: { year: 2017, month: 'february' }
-console.log(qdata.month); // returns 'february'
+  fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Added to log.');
+    }
+  });
+
+  if (q.pathname.includes('documentation')) {
+    filePath = ( './documentation.html');
+  } else {
+    filePath = './index.html';
+  }
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(data);
+    response.end();
+
+  });
+
+}).listen(8080);
+console.log('My test server is running on Port 8080.');
 
