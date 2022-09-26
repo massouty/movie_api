1d39844e5f573f9e591033674e95586f3d71c866
massouty <massouty@outlook.com>
2022-09-26 21:53:51 +0200

    added  new folders and files

---
+++ models.js
@@ -0,0 +1,28 @@
+const mongoose = require ("mongoose");
+let movieSchema = mongoose.Schema({Title:{type:String , required:true},
+Description:{type:String , required:true},
+Genre:{
+    Name:String,
+    Description:String
+},
+Director:{
+    Name:String,
+    Bio:String
+},
+ImagePath:String,
+Featured:Boolean
+});
+
+let userSchema = mongoose.Schema({userName:{type:String, required:true},
+password:{type:String , required:true},
+email:{type:String,required:true},
+Birthday: Date,
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+});
+
+let Movie = mongoose.model('Movie', movieSchema);
+let User = mongoose.model('User', userSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
+
